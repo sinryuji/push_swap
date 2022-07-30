4d168fdd6cd1922b9ea273c1eea40cd702fda0d6
sinryuji <siler2474@gmail.com>
2022-07-30 20:20:01 +0900

    fix: min_move_b logic improving // 5 value is fast, but 100 and 500 value is so slow

--- source/selection_sort.c
+++ source/selection_sort.c
@@ -6,7 +6,7 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 17:56:42 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/07/30 19:29:36 by hyeongki         ###   ########.fr       */
+/*   Updated: 2022/07/30 20:11:27 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@ void	min_move_b(t_stack **a, t_stack **b)
 {
 	t_stack *min_node;
 	int		min_index;
-	int		a_length;
 
 	min_node = get_min_node(*a);
 	min_index = get_node_index(min_node, *a);
 	while (min_index)
 	{
-		a_length = get_list_length(*a);
-		if ((a_length / min_index) <= (a_length / 2))
-			rra(a);
-		else
+		if (min_index <= get_list_length(*a) / 2)
 			ra(a);
+		else
+			rra(a);
+		if (check_sort(*a))
+			return ;
 		min_index = get_node_index(min_node, *a);
 	}
 	pb(a, b);
@@ -35,7 +35,13 @@ void	min_move_b(t_stack **a, t_stack **b)
 void	selection_sort(t_stack **a, t_stack **b)
 {
 	while (!check_sort(*a))
+	{
 		min_move_b(a, b);
+		if ((*a)->next->next)
+			if (((*a)->data > (*a)->next->data))
+				if ((*a)->data < (*a)->next->next->data)
+					sa(a);
+	}
 	while (*b)
 		pa(a, b);
 }
